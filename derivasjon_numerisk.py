f977f5b60bdaccaa295e4f719f8f84282b158943
Eirik Moltubak <eirikmalme@icloud.com>
2025-02-24 09:18:11 +0100

    Added double derivative

--- derivasjon_numerisk.py
+++ derivasjon_numerisk.py
@@ -7,13 +7,17 @@ def f(x):
 def f_derivative(x, h):
     return (f(x+h) - f(x))/h
 
+def f_2derivative(x, h):
+    return (f_derivative(x+h, h) - f_derivative(x, h))/h
+
 h = 0.01
 x = np.linspace(-10, 10, 100)
 
-plt.plot(x, f_derivative(x, h), label="f'(x) = 2x")
 plt.plot(x, f(x), label='f(x) = x^2')
+plt.plot(x, f_derivative(x, h), label="f'(x) = 2x")
+plt.plot(x, f_2derivative(x, h), label="f''(x) = 2")
 
-plt.title('Graph of x^2 and its derivative')
+plt.title('Graph of x^2 and its derivatives')
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('y')
