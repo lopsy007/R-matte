16c19f70657a9899c78e8b1b2beec20a428ec61d
Eirik Moltubak <eirikmalme@icloud.com>
2025-02-27 10:55:38 +0100

    Plotted e^(-x^2) and derivatives

--- e_x2_der_num_plot.py
+++ e_x2_der_num_plot.py
@@ -1,17 +1,18 @@
 import derivasjon_numerisk as plot
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
 
 
 x = np.linspace(-10, 10, 100)
-f = lambda x: x**3
+f = lambda x: math.e**(-x**2)
 
 
-plot.plot_same(x, f(x), 'f(x)=x^3')
-plot.plot_same(x, plot.f_derivative(x, 0.01, f), 'f\'(x)=3x^2')
-plot.plot_same(x, plot.f_2derivative(x, 0.01, f), 'f\'\'(x)=6x')
+plot.plot_same(x, f(x), 'f(x)=e^(-x^2)')	
+plot.plot_same(x, plot.f_derivative(x, 0.01, f), 'f\'(x)')
+plot.plot_same(x, plot.f_2derivative(x, 0.01, f), 'f\'\'(x)')
 plt.title('f(x), f\'(x) and f\'\'(x)')
 plt.legend()
 plt.show()
