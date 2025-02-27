b3fce8a1fd2e38a75e19aaf4cf658d3c39f7d154
Eirik Moltubak <eirikmalme@icloud.com>
2025-02-27 10:47:57 +0100

    Converted to importable module with methods

--- derivasjon_numerisk.py
+++ derivasjon_numerisk.py
@@ -1,52 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
-    return x**2
 
-def f_derivative(x, h):
+def f_derivative(x, h, f):
     return (f(x+h) - f(x))/h
 
-def f_2derivative(x, h):
-    return (f_derivative(x+h, h) - f_derivative(x, h))/h
+def f_2derivative(x, h, f):
+    return (f_derivative(x+h, h, f) - f_derivative(x, h, f))/h
 
-h = 0.01
-x = np.linspace(-10, 10, 100)
+def plot_sub(x, y, title, total_plots, plot_number, min_x=None, max_x=None, min_y=None, max_y=None):
+    plt.subplot(1, total_plots, plot_number)
+    plt.plot(x, y, label=title)
+    plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.axhline(0, color='black',linewidth=0.5)
+    plt.axvline(0, color='black',linewidth=0.5)
+    plt.xlim(-10, 10)
+    plt.ylim(-100, 100)
 
-plt.figure(figsize=(15, 5))
+def plot_same(x, y, title="f(x)", min_x=None, max_x=None, min_y=None, max_y=None):
+    plt.plot(x, y, label=title)
+    plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.axhline(0, color='black',linewidth=0.5)
+    plt.axvline(0, color='black',linewidth=0.5)
 
-plt.subplot(1, 3, 1)
-plt.plot(x, f(x), label='f(x) = x^2')
-plt.title('f(x) = x^2')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
-plt.xlim(-10, 10)
-plt.ylim(-20, 100)
-
-plt.subplot(1, 3, 2)
-plt.plot(x, f_derivative(x, h), label="f'(x) = 2x")
-plt.title("f'(x) = 2x")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
-plt.xlim(-10, 10)
-plt.ylim(-20, 100)
-
-plt.subplot(1, 3, 3)
-plt.plot(x, f_2derivative(x, h), label="f''(x) = 2")
-plt.title("f''(x) = 2")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
-plt.xlim(-10, 10)
-plt.ylim(-20, 100)
-
-plt.show()
-
+    if min_x and max_x:
+        plt.xlim(min_x, max_x)
+    if min_y and max_y:
+        plt.ylim(min_y, max_y)
