ddfdcd913a43affd521f2d91d6e0fa22f7d4f09d
Eirik Moltubak <eirikmalme@icloud.com>
2025-02-27 11:14:43 +0100

    Updated to use derivasjon_numerisk module

--- e_x2_der_num_plot.py
+++ e_x2_der_num_plot.py
@@ -1,18 +1,7 @@
 import derivasjon_numerisk as plot
-import numpy as np
-import matplotlib.pyplot as plt
 import math
 
 
-
-
-x = np.linspace(-10, 10, 100)
 f = lambda x: math.e**(-x**2)
+plot.plot_same_with_derivatives(f, -3, 3,)
 
-
-plot.plot_same(x, f(x), 'f(x)=e^(-x^2)')	
-plot.plot_same(x, plot.f_derivative(x, 0.01, f), 'f\'(x)')
-plot.plot_same(x, plot.f_2derivative(x, 0.01, f), 'f\'\'(x)')
-plt.title('f(x), f\'(x) and f\'\'(x)')
-plt.legend()
-plt.show()
