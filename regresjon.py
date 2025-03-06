b759687261e939c66681c5f0826463c95c84900e
Eirik Moltubak <eirikmalme@icloud.com>
2025-03-06 14:53:12 +0100

    Added another way to get coefficients

--- regresjon.py
+++ regresjon.py
@@ -1,10 +1,22 @@
 import plotting as plot
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+
+
+
 
 x = [1, 2, 3, 4, 5]
 y = [2.1, 3.9, 6.5, 7.1, 11.0]
 
+# Using scipy
+def f(x, a, b):
+    return a*x + b
+koeffisienter, kovarians = curve_fit(f, x, y)
+print(koeffisienter)
+
+
+# Using numpy
 coefficients = np.polyfit(x, y, 1)
 
 plt.scatter(x, y, color='red')
